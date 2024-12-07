e93b10f3a50c5fa0dcceac71f160afa2d1dec2f6
Jeff Farris <jeff.markues@gmail.com>
2024-12-06 22:46:56 -0500

    separate values into two arrays

--- day1/solution.js
+++ day1/solution.js
@@ -2,6 +2,16 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const node_fs_1 = require("node:fs");
 const dataString = (0, node_fs_1.readFileSync)('./day1/input', 'utf8');
-console.log(dataString.substring(0, 5));
 const splitStr = dataString.split(/\s+/g);
-console.log(`${splitStr[0]} ${splitStr[1]} ${splitStr[2]}`);
+let leftVals = [];
+let rightVals = [];
+splitStr.forEach((value, index) => {
+    if (index % 2 === 0) {
+        leftVals.push(parseInt(value));
+    }
+    else {
+        rightVals.push(parseInt(value));
+    }
+});
+console.log(leftVals[1]);
+console.log(rightVals[1]);
--- day1/solution.ts
+++ day1/solution.ts
@@ -2,8 +2,18 @@ import { readFileSync } from 'node:fs'
 
 const dataString = readFileSync('./day1/input', 'utf8');
 
-console.log(dataString.substring(0, 5));
-
 const splitStr = dataString.split(/\s+/g);
 
-console.log(`${splitStr[0]} ${splitStr[1]} ${splitStr[2]}`);
+let leftVals: Array<number> = [];
+let rightVals: Array<number> = [];
+
+splitStr.forEach((value, index) => {
+    if(index % 2 === 0) {
+        leftVals.push(parseInt(value));
+    } else {
+        rightVals.push(parseInt(value));
+    }
+});
+
+console.log(leftVals[1]);
+console.log(rightVals[1]);
