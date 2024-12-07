dc53781dc867ec9dd1e4851c2f405be3a426a79e
Jeff Farris <jeff.markues@gmail.com>
2024-12-07 17:06:07 -0500

    implement the rest of day 2 part 1

--- day2/solution.ts
+++ day2/solution.ts
@@ -4,7 +4,7 @@ type InputData = {
     reports: Array<Array<number>>;
 };
 
-function getInputArrays(): InputData {
+function getInputData(): InputData {
     let inputData: InputData = {
         reports: []
     }
@@ -22,4 +22,37 @@ function getInputArrays(): InputData {
     return inputData;
 }
 
-console.log(getInputArrays().reports[1]);
+function part1(): number {
+    let safeReports = 0;
+    const inputData = getInputData();
+
+    inputData.reports.forEach((report) => {
+        if(report[0] < report[1]) { // increasing
+            // Start at the first element and iterate to the next-to-last element
+            for(let i = 0; i < report.length - 1; i++) {
+                if(report[i] < report[i + 1] && report[i + 1] - report[i] > 0 && report[i + 1] - report[i] <= 3) {
+                    continue;
+                } else {
+                    return;
+                }
+            }
+            safeReports++;
+        } else if(report[0] > report[1]) { // decreasing
+            // Start at the first element and iterate to the next-to-last element
+            for(let i = 0; i < report.length - 1; i++) {
+                if(report[i] > report[i + 1] && report[i] - report[i + 1] > 0 && report[i] - report[i + 1] <= 3) {
+                    continue;
+                } else {
+                    return;
+                }
+            }
+            safeReports++;
+        } else { // unsafe report
+            return;
+        }
+    });
+
+    return safeReports;
+}
+
+console.log(part1());
