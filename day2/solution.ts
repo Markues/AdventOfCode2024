003be625462eef3cf10a582e84c40eae08a0fd85
Jeff Farris <jeff.markues@gmail.com>
2024-12-07 17:33:06 -0500

    add part two solution code

--- day2/solution.ts
+++ day2/solution.ts
@@ -27,9 +27,10 @@ function part1(): number {
     const inputData = getInputData();
 
     inputData.reports.forEach((report) => {
+        const len = report.length;
         if(report[0] < report[1]) { // increasing
             // Start at the first element and iterate to the next-to-last element
-            for(let i = 0; i < report.length - 1; i++) {
+            for(let i = 0; i < len - 1; i++) {
                 if(report[i] < report[i + 1] && report[i + 1] - report[i] > 0 && report[i + 1] - report[i] <= 3) {
                     continue;
                 } else {
@@ -39,7 +40,7 @@ function part1(): number {
             safeReports++;
         } else if(report[0] > report[1]) { // decreasing
             // Start at the first element and iterate to the next-to-last element
-            for(let i = 0; i < report.length - 1; i++) {
+            for(let i = 0; i < len - 1; i++) {
                 if(report[i] > report[i + 1] && report[i] - report[i + 1] > 0 && report[i] - report[i + 1] <= 3) {
                     continue;
                 } else {
@@ -55,4 +56,69 @@ function part1(): number {
     return safeReports;
 }
 
+function part2(): number {
+    let safeReports = 0;
+    const inputData = getInputData();
+
+    inputData.reports.forEach((report) => {
+        const len = report.length;
+        let dampener = 1;
+        if(report[0] < report[1]) { // increasing
+            // Start at the first element and iterate to the next-to-last element
+            for(let i = 0; i < len - 1; i++) {
+                // If the subsequent value is increasing and it is within 1-3 of the current value
+                if(report[i] < report[i + 1] && report[i + 1] - report[i] > 0 && report[i + 1] - report[i] <= 3) {
+                    continue;
+                } else {
+                    if(dampener === 1) {
+                        // If currently checking the final value(s)
+                        if(i === len - 2) {
+                            continue;
+                        }
+                        // If the next, next value is increasing and is within 1-3 of the current value
+                        else if(report[i] < report[i + 2] && report[i + 2] - report[i] > 0 && report[i + 2] - report[i] <= 3) {
+                            dampener--;
+                            i++;
+                            continue;
+                        }
+                    } else {
+                        return;
+                    }
+                }
+            }
+            // Only increment if all checks have passed
+            safeReports++;
+        } else if(report[0] > report[1]) { // decreasing
+            // Start at the first element and iterate to the next-to-last element
+            for(let i = 0; i < len - 1; i++) {
+                // If the subsequent value is decreasing and it is within 1-3 of the current value
+                if(report[i] > report[i + 1] && report[i] - report[i + 1] > 0 && report[i] - report[i + 1] <= 3) {
+                    continue;
+                } else {
+                    if(dampener === 1) {
+                        // If currently checking the final value(s)
+                        if(i === len - 2) {
+                            continue;
+                        } // If the next, next value is decreasing and is within 1-3 of the current value
+                        else if(report[i] > report[i + 2] && report[i] - report[i + 2] > 0 && report[i] - report[i + 2] <= 3) {
+                            dampener--;
+                            i++;
+                            continue;
+                        }
+                    } else {
+                        return;
+                    }
+                }
+            }
+            // Only increment if all checks have passed
+            safeReports++;
+        } else { // unsafe report
+            return;
+        }
+    });
+
+    return safeReports;
+}
+
 console.log(part1());
+console.log(part2());
