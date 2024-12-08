079c973bf7149120a81a054b85dc2f79afb7f7b6
Jeff Farris <jeff.markues@gmail.com>
2024-12-07 22:49:39 -0500

    more refactoring mostly in part 2

--- day2/solution.ts
+++ day2/solution.ts
@@ -22,6 +22,10 @@ function getInputData(): InputData {
     return inputData;
 }
 
+function isWithinThree(val: number): boolean {
+    return 3 - Math.abs(val) >= 0;
+}
+
 function part1(): number {
     let safeReports = 0;
     const inputData = getInputData();
@@ -34,12 +38,8 @@ function part1(): number {
 
         for(let i = 0; i < len - 1; i++) {
             let diff = report[i] - report[i + 1];
-            if((decreasing && diff > 0) || (!decreasing && diff < 0)) {
-                if(3 - Math.abs(diff) >= 0) {
-                    continue;
-                } else {
-                    return;
-                }
+            if(((decreasing && diff > 0) || (!decreasing && diff < 0)) && isWithinThree(diff)) {
+                continue;
             } else {
                 return;
             }
@@ -53,9 +53,6 @@ function part1(): number {
 function part2(): number {
     let safeReports = 0;
     const inputData = getInputData();
-    const isWithinThree = (val: number): boolean => {
-        return 3 - Math.abs(val) >= 0;
-    }
 
     inputData.reports.forEach((report) => {
         const len = report.length;
@@ -64,83 +61,48 @@ function part2(): number {
 
         if(report[0] < report[len - 1] || report[1] < report[len - 1]) { // increasing
             direction = -1;
-            // Start at the first element and iterate to the next-to-last element
-            for(let i = 0; i < len - 1; i++) {
-                let diff = report[i] - report[i + 1];
-                // If the subsequent value is increasing and it is within 1-3 of the current value
-                if(diff <= direction && isWithinThree(diff)) {
-                    continue;
-                } else {
-                    if(dampener === 1) {
-                        // If currently checking the final value(s)
-                        if(i === len - 2) {
-                            continue;
-                        }
-                        // If checking the first value, check second and third
-                        if(i === 0) {
-                            let secondDiff = report[i + 1] - report[i + 2];
-                            if(secondDiff <= direction && isWithinThree(secondDiff)) {
-                                dampener--;
-                                i++;
-                                continue;
-                            }
-                        }
-                        let skipDiff = report[i] - report[i + 2];
-                        // If the next, next value is increasing and is within 1-3 of the current value
-                        if(skipDiff <= direction && isWithinThree(skipDiff)) {
-                            dampener--;
-                            i++;
-                            continue;
-                        }
-                        return;
-                    } else {
-                        return;
-                    }
-                }
-            }
-            // Only increment if all checks have passed
-            safeReports++;
         } else if(report[0] > report[len - 1] || report[1] > report[len - 1]) { // decreasing
             direction = 1;
-            // Start at the first element and iterate to the next-to-last element
-            for(let i = 0; i < len - 1; i++) {
-                let diff = report[i] - report[i + 1];
-                // If the subsequent value is decreasing and it is within 1-3 of the current value
-                if(diff >= direction && isWithinThree(diff)) {
-                    continue;
-                } else {
-                    if(dampener === 1) {
-                        // If currently checking the final value(s)
-                        if(i === len - 2) {
-                            continue;
-                        }
-                        // If checking the first value, check second and third
-                        if(i === 0) {
-                            let secondDiff = report[i + 1] - report[i + 2];
-                            if(secondDiff >= direction && isWithinThree(secondDiff)) {
-                                dampener--;
-                                i++;
-                                continue;
-                            }
-                        }
-                        let skipDiff = report[i] - report[i + 2];
-                        // If the next, next value is decreasing and is within 1-3 of the current value
-                        if(skipDiff >= direction && isWithinThree(skipDiff)) {
-                            dampener--;
-                            i++;
-                            continue;
-                        } 
-                        return;
-                    } else {
-                        return;
-                    }
-                }
-            }
-            // Only increment if all checks have passed
-            safeReports++;
         } else { // unsafe report
             return;
         }
+
+        // Start at the first element and iterate to the next-to-last element
+        for(let i = 0; i < len - 1; i++) {
+            let diff = report[i] - report[i + 1];
+            // If the subsequent value is increasing and it is within 1-3 of the current value
+            if(((diff <= direction && direction === -1) || (diff >= direction && direction === 1)) && isWithinThree(diff)) {
+                continue;
+            } else {
+                if(dampener === 1) {
+                    // If currently checking the final value(s)
+                    if(i === len - 2) {
+                        continue;
+                    }
+                    // If checking the first value, check second and third
+                    if(i === 0) {
+                        let secondDiff = report[i + 1] - report[i + 2];
+                        if(((secondDiff <= direction && direction === -1) || (secondDiff >= direction && direction === 1)) && isWithinThree(secondDiff)) {
+                            dampener--;
+                            i++;
+                            continue;
+                        }
+                    }
+                    let skipDiff = report[i] - report[i + 2];
+                    // If the next, next value is increasing and is within 1-3 of the current value
+                    if(((skipDiff <= direction && direction === -1) || (skipDiff >= direction && direction === 1)) && isWithinThree(skipDiff)) {
+                        dampener--;
+                        i++;
+                        continue;
+                    }
+                    return;
+                } else {
+                    return;
+                }
+            }
+        }
+        // Only increment if all checks have passed
+        safeReports++;
     });
 
     return safeReports;
